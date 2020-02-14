ed162ff56302dd3d3c98fbaa95d39a58a37b833c
Daniel Azevêdo <48658061+DanielAzevedoM@users.noreply.github.com>
2020-02-14 15:18:02 -0300

    Criação FileController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ import multer from 'multer';
 
 import multerConfig from './config/multer';
 
+import FileController from './app/controllers/FileController';
+
 import UserController from './app/controllers/UserController';
 
 import SessionController from './app/controllers/SessionController';
@@ -20,8 +22,6 @@ routes.post('/sessions', SessionController.store);
 
 routes.put('/users', authMiddlewares, UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
