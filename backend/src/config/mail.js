3009b70d3683fd5398cf2e0d50ddbcc615603cb6
Daniel Azevedo <danielazevedom@hotmail.com>
2020-03-12 21:34:04 -0300

    adicionado config nodemailer

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,12 +1,14 @@
 export default {
-  host: '',
-  port: '',
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: 'a6af6fdf1d493b',
+    pass: '47cf0cc6d40463',
   },
   default: {
     From: 'Equipe Gobarber <noreply@gobarber.com',
   },
 };
+
+/* Host de email utilizado mailtrap, site "mailtrap.io" */
